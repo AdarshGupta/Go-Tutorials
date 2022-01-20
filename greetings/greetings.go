3f635e086cac9327c11fb3cfbc74da5b442fe553
AdarshGupta <adarsh035@gmail.com>
2022-01-20 20:41:33 +0530

    Error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // In Go, a function whose name starts with a capital letter can be called by a function not in the same package. 
 // This is known in Go as an exported name.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+
+	// Go uses the value on the right to determine the variable's type
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
