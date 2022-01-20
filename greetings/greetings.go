3bd16cbefce6c7576cf43aabd1298db43c85c8d9
AdarshGupta <adarsh035@gmail.com>
2022-01-20 21:04:26 +0530

    Use math random

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
+	"time"
 )
 
 // In Go, a function whose name starts with a capital letter can be called by a function not in the same package. 
@@ -10,11 +12,30 @@ import (
 func Hello(name string) (string, error) {
 	// If no name was given, return an error with a message
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
 	}
 
-
-	// Go uses the value on the right to determine the variable's type
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+// Go executes init functions automatically at program startup, after global variables have been initialized. 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+// randomFormat starts with a lowercase letter, making it accessible only to code in its own package (in other words, it's not exported).
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
